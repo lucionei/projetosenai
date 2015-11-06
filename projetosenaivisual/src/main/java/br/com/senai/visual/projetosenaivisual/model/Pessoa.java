04fc0b8fe45d8194c7168dc9ba56469d3e1b675c
Lucionei <Lucionei@172.20.51.210>
2015-11-06 21:26:34 -0200

    Teste de alteração

---
+++ projetosenaivisual/src/main/java/br/com/senai/visual/projetosenaivisual/model/Pessoa.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.senai.visual.projetosenaivisual.model;
+
+/**
+ *
+ * @author Lucionei
+ */
+public class Pessoa {
+    
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    
+    
+}
