215b9a727e6ecce8b81cf3b04d1c3b06cf1ed794
Lucionei <Lucionei@172.20.51.210>
2015-11-06 22:16:37 -0200

    Teste de alteração

--- projetosenaivisual/src/main/java/br/com/senai/visual/projetosenaivisual/model/Pessoa.java
+++ projetosenaivisual/src/main/java/br/com/senai/visual/projetosenaivisual/model/Pessoa.java
@@ -5,16 +5,36 @@
  */
 package br.com.senai.visual.projetosenaivisual.model;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  *
  * @author Lucionei
  */
-
-public class Pessoa {
-    
+@Entity
+@Table(name="PESSOA")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@XmlRootElement
+public abstract class Pessoa implements Serializable {
+   
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
     private Long id;
+    @Column
     private String nome;
+    @Column
     private String email;
+    @Column
     private String telefone;
     
     
